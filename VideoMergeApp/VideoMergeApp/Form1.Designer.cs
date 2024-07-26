cb8c7595dc33202b5cccefa8f9a07aacc9e4210e
Furkan Usta <92405866+Furkn-usta@users.noreply.github.com>
2024-07-26 14:04:41 +0300

    Add files via upload
    "VideoMergeApp\bin\Debug" ffmpeg.exe must be added to the file location.

---
+++ VideoMergeApp/VideoMergeApp/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace VideoMergeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(539, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 134);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(539, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Video Choose";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(536, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Video";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-1, 1);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(522, 449);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(536, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Voice ";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(539, 220);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(250, 43);
+            this.listBox2.TabIndex = 6;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(539, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Voice Choose";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(-1, 463);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(522, 47);
+            this.axWindowsMediaPlayer2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(663, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Video Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(663, 269);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Voice Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(539, 298);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(250, 58);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Video Merge";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(539, 379);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(249, 54);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(536, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Log";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(536, 440);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Location";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(539, 456);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(249, 54);
+            this.richTextBox2.TabIndex = 14;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 515);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VideoMergeApp";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ VideoMergeApp/VideoMergeApp/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxWMPLib;
+//C:\Users\EnkayPC\AppData\Local\Temp Log Location
+namespace VideoMergeApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            axWindowsMediaPlayer1.PlayStateChange += new _WMPOCXEvents_PlayStateChangeEventHandler(axWindowsMediaPlayer1_PlayStateChange);
+            axWindowsMediaPlayer2.PlayStateChange += new _WMPOCXEvents_PlayStateChangeEventHandler(axWindowsMediaPlayer2_PlayStateChange);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "MP4 Files|*.mp4|All Files|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "MP3 Files|*.mp3|All Files|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    listBox2.Items.Add(file);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                PlaySelectedVideo();
+                if (listBox2.Items.Count >= 1 && listBox1.SelectedIndex == 0)
+                {
+                    string selectedFile = listBox2.Items[listBox2.Items.Count - 1].ToString();
+                    axWindowsMediaPlayer2.URL = selectedFile;
+                    axWindowsMediaPlayer2.Ctlcontrols.play();
+                }
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                PlaySelectedAudio();
+            }
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, _WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 8) // MediaEnded state
+            {
+                PlayNextVideo();
+            }
+        }
+
+        private void axWindowsMediaPlayer2_PlayStateChange(object sender, _WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 8) // MediaEnded state
+            {
+                PlayNextAudio();
+            }
+        }
+
+        private void PlaySelectedVideo()
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedFile = listBox1.SelectedItem.ToString();
+                axWindowsMediaPlayer1.URL = selectedFile;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private async void PlayNextVideo()
+        {
+            int currentIndex = listBox1.SelectedIndex;
+            if (currentIndex >= 0 && currentIndex < listBox1.Items.Count - 1)
+            {
+                listBox1.SelectedIndex = currentIndex + 1;
+                string nextFile = listBox1.Items[listBox1.SelectedIndex].ToString();
+                axWindowsMediaPlayer1.URL = nextFile;
+                await Task.Delay(500);  // Wait for half a second
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void PlaySelectedAudio()
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                string selectedFile = listBox2.SelectedItem.ToString();
+                axWindowsMediaPlayer2.URL = selectedFile;
+                axWindowsMediaPlayer2.Ctlcontrols.play();
+            }
+        }
+
+        private void PlayNextAudio()
+        {
+            int currentIndex = listBox2.SelectedIndex;
+            if (currentIndex >= 0 && currentIndex < listBox2.Items.Count - 1)
+            {
+                listBox2.SelectedIndex = currentIndex + 1;
+                string nextFile = listBox2.Items[listBox2.SelectedIndex].ToString();
+                axWindowsMediaPlayer2.URL = nextFile;
+                axWindowsMediaPlayer2.Ctlcontrols.play();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Hepsini Silmek İçin Evet, Seçili Satırı Silmek İçin Hayır'a Basınız...",
+                    "Silme İşlemi",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button3);
+
+                if (result == DialogResult.Yes)
+                {
+                    listBox1.Items.Clear();
+                }
+                else if (result == DialogResult.No)
+                {
+                    if (listBox1.SelectedIndex != -1)
+                    {
+                        listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lütfen silmek için bir satır seçin.", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Listede silinecek öğe bulunmuyor.", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count > 0)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Hepsini Silmek İçin Evet, Seçili Satırı Silmek İçin Hayır'a Basınız...",
+                    "Silme İşlemi",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button3);
+
+                if (result == DialogResult.Yes)
+                {
+                    listBox2.Items.Clear();
+                }
+                else if (result == DialogResult.No)
+                {
+                    if (listBox2.SelectedIndex != -1)
+                    {
+                        listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lütfen silmek için bir satır seçin.", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Listede silinecek öğe bulunmuyor.", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("ListBox1'de video bulunmuyor.", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "MP4 Files|*.mp4",
+                Title = "Videoyu Kaydet"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string outputPath = saveFileDialog.FileName;
+
+                string tempDir = Path.Combine(Path.GetTempPath(), "VideoTemp");
+                Directory.CreateDirectory(tempDir);
+
+                try
+                {
+                    string videoListPath = Path.Combine(tempDir, "videoList.txt");
+                    using (StreamWriter writer = new StreamWriter(videoListPath))
+                    {
+                        foreach (string videoFile in listBox1.Items)
+                        {
+                            writer.WriteLine($"file '{videoFile.Replace("\\", "/")}'");
+                        }
+                    }
+
+                    string mergedVideoPath = Path.Combine(tempDir, "merged.mp4");
+                    string concatCommand = $"-f concat -safe 0 -i \"{videoListPath}\" -c copy \"{mergedVideoPath}\"";
+                    ExecuteFFmpegCommand(concatCommand, "Video Birleştirme");
+
+                    if (listBox2.Items.Count > 0)
+                    {
+                        string audioFile = listBox2.Items[0].ToString().Replace("\\", "/");
+                        string addAudioCommand = $"-i \"{mergedVideoPath}\" -i \"{audioFile}\" -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 -strict experimental \"{outputPath}\"";
+                        ExecuteFFmpegCommand(addAudioCommand, "Ses Ekleme");
+                    }
+                    else
+                    {
+                        File.Move(mergedVideoPath, outputPath);
+                        MessageBox.Show("Sadece video başarıyla birleştirildi!", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    Directory.Delete(tempDir, true);
+                    MessageBox.Show("Video Başarıyla Kayıt Edildi! Video Konumu:"+ outputPath.ToString(), "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    richTextBox2.Text = outputPath.ToString();
+                }
+            }
+        }
+
+        private void ExecuteFFmpegCommand(string arguments, string processDescription)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "ffmpeg.exe",
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+
+                string output = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                // Error Message Saving
+                string logFilePath = Path.Combine(Path.GetTempPath(), $"ffmpeg_log_{processDescription}.txt");
+                File.WriteAllText(logFilePath, "Error Output:\n" + output);
+                richTextBox1.Text = logFilePath.ToString();
+                /*
+                if (process.ExitCode != 0)
+                {
+                    throw new Exception($"FFmpeg komutu başarısız oldu: {output}\nDetaylar için log dosyasını kontrol edin: {logFilePath}");
+                }
+                else
+                {
+                    MessageBox.Show($"FFmpeg komut çıktı:\n{output}\nDetaylar için log dosyasını kontrol edin: {logFilePath}", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                  
+                }
+                */
+            }
+        }
+
+
+
+    }
+}
